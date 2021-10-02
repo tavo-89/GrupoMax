5996847bffb99ab4f7968b0d6fa694964a94eb27
tavo-89 <gusacosta.87@gmail.com>
2021-10-02 01:12:51 -0300

    modificacion de axios

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -9,23 +9,20 @@ function Form() {
     console.log(data);
     const token ="eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiZGV2ZWxvcGVyIiwiSXNzdWVyIjoiSXNzdWVyIiwiVXNlcm5hbWUiOiJGcm9udERldiIsImV4cCI6MTYyODY5NTg3NiwiaWF0IjoxNjI4Njk1ODc2fQ.CDRPz6Eta78BzmuNTNZsnzzDU2TRgvEtMs-_aZlWCZQ";
 
-    data = JSON.stringify({});
+    data = JSON.stringify(data);
 
     var config = {
-      method: "post",
-      url: "https://sistemacaliva.com/api/front-test",
+      method: 'post',
+      url: 'https://sistemacaliva.com/api/front-test',
       headers: {
-        "test-key":
-          token,
-        "Content-Type": "application/json",
-        Cookie:
-          "XSRF-TOKEN=eyJpdiI6Ino0NUowb1c1WlcxNEZmY1h3ckpTdVE9PSIsInZhbHVlIjoiVDdDOXJtKzhXbFBwYjNIWUVVY09UWFlaRkgyNDZCY2kzU3U1dm83R2hWbzNNQ2laWFVNaTl3dWl5WlVqc1RNVnNEaE9Ia2xMTk9uNEtkelwvWXAyb2RRPT0iLCJtYWMiOiI5MjZlMzRlNDQ0ZmE4NmEwNjJkODE1ZjdkYzQ1OGZjMzZhYTA5YjNkMjU0NGZlYjdkNjYwYzNiYTI5M2VjYWI1In0%3D; laravel_session=eyJpdiI6IkMzdGg5TVwvS1RCVzRaK0pZVjV4V0JnPT0iLCJ2YWx1ZSI6Im9WRUpJRzJudmdMaklDS3p2RTdYZFVXNXk2N0QxRzB4aU1JM2JoTEpma0pWMTNnUVdxWEd0TzJadTBKTzNsbm11bU5yU0o4ektoanpXUVdRV21PSUh3PT0iLCJtYWMiOiIzN2ZhMTY0YzA1OTQxNWRiZmZkMDQ3YWNkY2NhNTAyNGQ0ZDAyMTkyZmYwMTA0ZDMwODhjZDllMGUyNjVmMWM2In0%3D",
+        'test-key': token,
       },
       data: data,
     };
 
     Axios(config)
       .then(function (response) {
+        console.log('response del service');
         console.log(JSON.stringify(response.data));
       })
       .catch(function (error) {
